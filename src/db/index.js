6ac2a22f51cb98b02a4751fb6aae4b997185cbe3
DawoodIfzaal <dawoodifzal.di2@gmail.com>
2025-06-10 12:50:07 +0530

    add server with port

--- src/db/index.js
+++ src/db/index.js
@@ -3,8 +3,8 @@ import { DB_NAME } from "../constants.js"
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-    console.log("MongoDB connected successfully")
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    console.log("MongoDB connected successfully, DB_HOST = ", connectionInstance.connection.host)
 
   } catch (error) {
     console.log("MongoDB Connection Error", error)
